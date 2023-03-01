e6c34b615d0bc1b919c07b55d64b071390f34b5d
Christian Cesar <christiancnp@gmail.com>
2023-02-28 20:37:41 -0400

    feat: implementing mrp service

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,19 @@ import BudgetHuntedMapper from "@shared/database/mappers/BudgetHuntedMapper";
 import { createJsonFile } from "@shared/helpers/createJsonFile";
 import ProductStockHuntedMapper from "@shared/database/mappers/ProductStockHuntedMapper";
 import { ProductStockHuntedDTO } from "./dtos/ProductStockHuntedDTO";
+import { ManufacturingResourcePlanning } from "./services/ManufacturingResourcePlanning";
+import { ProductStockDTO } from "./dtos/domain/ProductStockDTO";
+import { BudgetDTO } from "./dtos/domain/BudgetDTO";
 
 (async () => {
-  const url = "https://sistema.wvetro.com.br/wvetro/app.wvetro.login";
-  const budgetHunter = new BudgetHunter({
-    user: process.env.WVETRO_USER,
-    password: process.env.WVETRO_PASSWORD,
-    license: process.env.WVETRO_LICENSE,
-    url,
-  });
-  await budgetHunter.load();
+  // const url = "https://sistema.wvetro.com.br/wvetro/app.wvetro.login";
+  // const budgetHunter = new BudgetHunter({
+  //   user: process.env.WVETRO_USER,
+  //   password: process.env.WVETRO_PASSWORD,
+  //   license: process.env.WVETRO_LICENSE,
+  //   url,
+  // });
+  // await budgetHunter.load();
   // await budgetHunter.getBudgets();
   // await budgetHunter.getBudgetItems();
 
@@ -27,17 +30,35 @@ import { ProductStockHuntedDTO } from "./dtos/ProductStockHuntedDTO";
   // });
   // createJsonFile("budgets_normalize", budgetsNormalize);
 
-  const productStockHunted = await budgetHunter.getProductStock();
-  createJsonFile("product_stock", productStockHunted);
+  // const productStockHunted = await budgetHunter.getProductStock();
+  // createJsonFile("product_stock", productStockHunted);
 
-  const producStockFile = fs.readFileSync("product_stock.json", {
-    encoding: "utf8",
+  // const producStockFile = fs.readFileSync("product_stock.json", {
+  //   encoding: "utf8",
+  // });
+  // const producStock = JSON.parse(producStockFile);
+  // const producStockNormalized = producStock.map(
+  //   (productStock: ProductStockHuntedDTO) => {
+  //     return ProductStockHuntedMapper.toDomain(productStock);
+  //   }
+  // );
+  // createJsonFile("product_stock_normalized", producStockNormalized);
+
+  const mrpService = new ManufacturingResourcePlanning();
+  const budgets = JSON.parse(
+    fs.readFileSync("budgets_normalize.json", {
+      encoding: "utf8",
+    })
+  ) as BudgetDTO[];
+
+  const productStock = JSON.parse(
+    fs.readFileSync("product_stock_normalized.json", {
+      encoding: "utf8",
+    })
+  ) as ProductStockDTO[];
+
+  mrpService.execute({
+    budgets,
+    productStock,
   });
-  const producStock = JSON.parse(producStockFile);
-  const producStockNormalized = producStock.map(
-    (productStock: ProductStockHuntedDTO) => {
-      return ProductStockHuntedMapper.toDomain(productStock);
-    }
-  );
-  createJsonFile("product_stock_normalized", producStockNormalized);
 })();
--- src/services/ManufacturingResourcePlanning.ts
+++ src/services/ManufacturingResourcePlanning.ts
@@ -1,32 +1,84 @@
+import { createJsonFile } from "@shared/helpers/createJsonFile";
 import { BudgetDTO } from "src/dtos/domain/BudgetDTO";
 import { BudgetItemsDTO } from "src/dtos/domain/BudgetItemsDTO";
 import { ProductStockDTO } from "src/dtos/domain/ProductStockDTO";
+import { StillDTO } from "src/dtos/domain/StillDTO";
 
 type ManufacturingResourcePlanningParams = {
   budgets: BudgetDTO[];
-  product_stock: ProductStockDTO[];
+  productStock: ProductStockDTO[];
 };
 
+type StillGroup = {
+  [key: string]: StillDTO[];
+};
+
+type StillCount = {
+  [key: string]: {
+    [key: string]: number;
+  };
+};
 export class ManufacturingResourcePlanning {
   public execute({
     budgets,
-    product_stock,
+    productStock,
   }: ManufacturingResourcePlanningParams) {
-    const budgetBilledFiltered = budgets.filter((budget) => {
-      budget.status_budget === "billed";
+    const budgetBilledFiltered = budgets.filter(
+      (budget) => budget.status_budget === "billed"
+    );
+
+    const budgetNotCompleted = budgetBilledFiltered.filter((budget) => {
+      const statusProducerExist = budget.status_producer.match(/\d+/g);
+
+      if (statusProducerExist) {
+        const statusProducer = Number(statusProducerExist[0]);
+
+        if (statusProducer < 10) {
+          return budget;
+        }
+      }
     });
 
-    const BudgetNotCompleted = budgetBilledFiltered.filter((budget) => {
-      budget.status_producer.match(/complete/g);
-    });
+    const budgetStill: StillDTO[] = [];
+    for (const budget of budgetNotCompleted) {
+      if (budget.costs?.still) {
+        budgetStill.push(...budget.costs.still);
+      }
+    }
 
-    const budgetItems: BudgetItemsDTO[] = [];
+    const stillGrouped = budgetStill.reduce((stillPrevius, stillCurrent) => {
+      const key = stillCurrent["code"];
+      if (!stillPrevius[key]) {
+        stillPrevius[key] = [] as StillGroup[];
+      }
+      stillPrevius[key].push(stillCurrent);
 
-    BudgetNotCompleted.forEach((budget) => {
-      budgetItems.push(...budget.items);
-    });
+      return stillPrevius as StillGroup;
+    }, {} as StillGroup);
 
-    //Unir produtos iguais
-    const budgetItemsFiltered = budgetItems.some((budgetItem) => {});
+    const keys = Object.keys(stillGrouped);
+    const stillCounted = {};
+    for (const key of keys) {
+      // console.log(stillGrouped[key]);
+      const still = (stillGrouped[key] as StillDTO[]).reduce((acc, obj) => {
+        const key = obj["code"];
+        if (!acc[key]) {
+          acc[key] = [];
+        }
+        const colorKey = obj["color"];
+
+        if (!acc[key][colorKey]) {
+          acc[key][colorKey] = 0;
+        }
+
+        acc[key][colorKey] += obj["quantity"];
+        return acc;
+      }, {} as StillCount);
+      Object.assign(stillCounted, still);
+    }
+    console.log(stillCounted);
+
+    // console.log(stillCounted);
+    createJsonFile("still_counted", stillCounted);
   }
 }
